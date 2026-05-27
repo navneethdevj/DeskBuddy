1b72fcedea3e4fc18c30bbf4db184d6eeafca0bb
navneethdevj <chn25cse576@ceconline.edu>
2026-05-27 14:31:45 +0530

    Change session timer mode to 'breakInterval'

--- deskbuddy/components/settings.js
+++ deskbuddy/components/settings.js
@@ -32,7 +32,7 @@ const Settings = (() => {
     nightAutoVolume: true,
     sessionLength:   25,
     timerStep:       5,
-    sessionTimerMode: 'remaining',
+    sessionTimerMode: 'breakInterval',
     ticksEnabled:       true,
     celebrationEnabled: true,
     breakAnimEnabled:   true,
